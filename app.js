d489f0241ff2cb424d16d036fa71091c3ce053a6
Chauncey <Chaunceyplum@gmail.com>
2023-10-21 21:55:49 -0400

    fixed get lambda function to an extent

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-var express = require('express');
-var app = express();
+var express = require('express')
+var app = express()
+const { Client } = require('pg')
 
-app.get('/', function(req, res) {
+const client = new Client({
+  user: 'postgres',
+  host: 'ec2-18-188-115-177.us-east-2.compute.amazonaws.com',
+  database: 'postgres',
+  password: 'post123',
+  port: 5432,
+})
+client.connect(function (err) {
+  if (err) throw err
+  console.log('Connected!')
+})
+
+app.get('/', async (req, res) => {
+  const userpostView = await client.query('SELECT * FROM public.userpost')
+
   res.send({
-    "Output": "Hello World!"
-  });
-});
+    data: `${userpostView}`,
+  })
+})
 
-app.post('/', function(req, res) {
+app.post('/', function (req, res) {
   res.send({
-    "Output": "Hello World!"
-  });
-});
-
+    Output: 'Hello World!',
+  })
+})
 
 // Export your Express configuration so that it can be consumed by the Lambda handler
 module.exports = app
